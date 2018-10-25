07c03f015618b69e3664b005c0c8a5e78806a0c7
Alex Goulart <goowlart@gmail.com>
2018-10-25 11:46:39 +0200

    La fonction "Magic Inc"

--- functions/affichage 50.js
+++
@@ -1,4 +0,0 @@
-
-for (var i = 1; i < 51; i++) {
-	console.log("j'ai dit bonjour ");
-}
---
+++ functions/magicInc.js
@@ -0,0 +1,65 @@
+var test = magicInc(0.5,'inc');
+
+function magicInc(valeur,srt) {
+	var n = valeur;
+    var inc = srt;
+	var f = n.toString().length * 100 / 100; //size of my variable n 
+
+	function positive() {
+		let str = 1;
+		for (let i = 1; i < f; i++) {
+			str = str + '0';
+		}
+		return str
+	}
+
+	function negative() {
+		let str = 1;
+		for (let i = 1; i < f; i++) {
+			str = str + '0';
+		}
+		return str
+	}
+
+	switch (srt) {
+		case 'inc':
+			if (n < -0.1 ) {
+				n += 0.1;
+				n = n.toFixed(1);
+			} else if (n >= 0.1 && n < 1.0) {
+				n += 0.1;
+				n = n.toFixed(1);
+			} else if (n > 0 && n <= 9) {
+				n += 1;
+			} else if (n >= 10) {
+				let str = positive(n)
+				n = (parseInt(n / str, 10) + 1) * str;
+			}
+			console.log(n);
+			break;
+		case 'dec':
+			if (n > 0.1 && n < 1) {
+				n -= 0.1;
+
+			} else if (n >= 1 && n <= 9) {
+				n -= 1;
+
+			} else if (n <= -1 && n > -10) {
+				n -= 1;
+
+			} else if (n < -0.1 && n >= -10) {
+				n -= 0.1;
+				n = n.toFixed(1);
+
+			} else if (n <= -10) {
+				n -= 10;
+			} else if (n >= 10) {
+				let str = negative(n);
+				n = (parseInt(n / str, 10) + 1) * str;
+			}
+			console.log(n);
+			break;
+		default:
+			console.log(n);
+	}
+}
