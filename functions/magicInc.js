b961ec30a634a576d9bd8542ccf96672b2168466
Alex Goulart <goowlart@gmail.com>
2018-10-25 11:59:51 +0200

    square

--- functions/magicInc.js
+++ functions/magicInc.js
@@ -1,18 +1,10 @@
-var test = magicInc(0.5,'inc');
+var test = magicInc(0.8, 'inc');
 
-function magicInc(number,str) {
+function magicInc(number, str) {
 	var n = number;
 	var f = n.toString().length * 100 / 100; //size of my variable n 
 
-	function positive() {
-		let str = 1;
-		for (let i = 1; i < f; i++) {
-			str = str + '0';
-		}
-		return str
-	}
-
-	function negative() {
+	function square() {
 		let str = 1;
 		for (let i = 1; i < f; i++) {
 			str = str + '0';
@@ -22,7 +14,7 @@ function magicInc(number,str) {
 
 	switch (str) {
 		case 'inc':
-			if (n < -0.1 ) {
+			if (n < -0.1) {
 				n += 0.1;
 				n = n.toFixed(1);
 			} else if (n >= 0.1 && n < 1.0) {
@@ -31,7 +23,7 @@ function magicInc(number,str) {
 			} else if (n > 0 && n <= 9) {
 				n += 1;
 			} else if (n >= 10) {
-				let str = positive(n)
+				let str = square(n)
 				n = (parseInt(n / str, 10) + 1) * str;
 			}
 			console.log(n);
@@ -53,7 +45,7 @@ function magicInc(number,str) {
 			} else if (n <= -10) {
 				n -= 10;
 			} else if (n >= 10) {
-				let str = negative(n);
+				let str = square(n);
 				n = (parseInt(n / str, 10) + 1) * str;
 			}
 			console.log(n);
